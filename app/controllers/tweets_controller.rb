24d3abcb36d8d8ffabb33f00c7cd2d7d6411c04f
Jorge Soto <djsmoothzouk@gmail.com>
2021-02-23 17:15:12 -0600

    Can POST new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
+
+    before_action :get_tweet, only: [:show, :update, :destroy]
+
     def index
         render json: {status: 200, tweets: Tweet.all}
     end
@@ -6,5 +9,26 @@ class TweetsController < ApplicationController
     def show
         tweet = Tweet.find(params[:id])
         render(json: { tweet: tweet })
+    end
+
+    def create
+        tweet = Tweet.new(tweet_params)
+    
+        if tweet.save
+           render status: 201, json: { tweet: tweet }
+        else
+           render(status: 422, json: { tweet: tweet, errors: tweet.errors })
+        end
+      end
+
+      
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:title, :content, :author)
+    end
+
+    def get_tweet
+        @book = Tweet.find(params[:id])
     end
 end
